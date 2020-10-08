b5eac50ef568ba1332a4ef35ac61772991c53707
ivang <35350256+ivanMSC@users.noreply.github.com>
2020-10-08 11:31:46 -0300

    Ahora no elimina Usados, solo "Internacional" #1

--- src/content_script.js
+++ src/content_script.js
@@ -2,7 +2,9 @@ window.addEventListener ("click", myMain, false);
 function myMain () {
 	var element = document.querySelectorAll('span.ui-search-item__group__element.ui-search-item__details');
 	for(var i=0;i<element.length;i++){
+           if(element[i].innerHTML == "Internacional"){
 		element[i].closest("li").remove();
 		//element[i].parentElement.remove()
 		}
+            }
 }
