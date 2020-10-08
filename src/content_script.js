4e4a5d6278576bed5dd95fdef1d4b6bd29b59e34
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2020-10-08 19:37:34 -0300

    No necesitar hacer click

--- src/content_script.js
+++ src/content_script.js
@@ -1,10 +1,19 @@
-window.addEventListener ("click", myMain, false);
-function myMain () {
-	var element = document.querySelectorAll('span.ui-search-item__group__element.ui-search-item__details');
-	for(var i=0;i<element.length;i++){
-           if(element[i].innerHTML == "Internacional"){
-		element[i].closest("li").remove();
-		//element[i].parentElement.remove()
-		}
-            }
-}
+(function(d, w) {
+  function load () {
+    // Obtener listado de artículos internacionales
+    let elements = Array.from(d.querySelectorAll('span.ui-search-item__group__element.ui-search-item__details'));
+    elements = elements.filter(e => e.textContent === "Internacional");
+
+    // Eliminar artículos si hay
+    if (elements.length > 0) {
+      elements.forEach(e => {
+        e.closest("li").remove();
+      });
+      
+      // Ejecutar hasta que ya no queden artículos intl
+      setTimeout(load, 500);
+    }
+  }
+  
+  w.addEventListener('load', load);
+})(document, window);
