6c5343eede57eddafa7978a55c315b928f8c8c7b
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2020-10-09 01:18:32 -0300

    Ordenar y volver a método de selección anterior
    Apuntar nuevamente a los flairs (e.g., "Internacional"), en vez de a cada item `<li>`.

--- src/content_script.js
+++ src/content_script.js
@@ -1,25 +1,21 @@
 (function(d, w) {
   function load () {
-    let results = Array.from(d.querySelectorAll('li.ui-search-layout__item'));
-    let changed = false;
+    // Apuntar al flair del tipo de productos, filtrar por los que dicen "Internacional",
+    // obtener el <li> parent, y filtrar los que no hayan sido ocultados.
+    let results = Array.from(d.querySelectorAll('span.ui-search-item__details'))
+      .filter(i => i.textContent === 'Internacional')
+      .map(j => j.closest('li.ui-search-layout__item'))
+      .filter(k => k.style.display !== "none");
     
-    for (let result of results) {
-      if (result.dataset.hidden) continue;
-      
-      let label = result.querySelector('span.ui-search-item__details');
-      if (label && label.textContent === 'Internacional') {
-        result.style.display = "none";
-        result.dataset.hidden = true;
-        changed = true;
-      }
-    }
-    
-    if (changed) {
+    // Si se encontraron productos internacionales, ocultarlos y volver a llamar
+    if (results.length > 0) {
+      results.forEach(i => { i.style.display = "none" });
       setTimeout(load, 500);
     }
   }
   
+  // Cargar sólo si es una página de resultados
   if (d.querySelector('section.ui-search-results')) {
-    w.addEventListener('load', load);
+    load();
   }
 })(document, window);
