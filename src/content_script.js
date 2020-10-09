7ac6dbdc9cd1103a86b88285d004168cbab1bdab
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2020-10-08 23:34:32 -0300

    No eliminar, solo ocultar los resultados
    Además, solo ejecutar si es una página de resultados

--- src/content_script.js
+++ src/content_script.js
@@ -1,19 +1,25 @@
 (function(d, w) {
   function load () {
-    // Obtener listado de artículos internacionales
-    let elements = Array.from(d.querySelectorAll('span.ui-search-item__group__element.ui-search-item__details'));
-    elements = elements.filter(e => e.textContent === "Internacional");
-
-    // Eliminar artículos si hay
-    if (elements.length > 0) {
-      elements.forEach(e => {
-        e.closest("li").remove();
-      });
+    let results = Array.from(d.querySelectorAll('li.ui-search-layout__item'));
+    let changed = false;
+    
+    for (let result of results) {
+      if (result.dataset.hidden) continue;
       
-      // Ejecutar hasta que ya no queden artículos intl
+      let label = result.querySelector('span.ui-search-item__details');
+      if (label && label.textContent === 'Internacional') {
+        result.style.display = "none";
+        result.dataset.hidden = true;
+        changed = true;
+      }
+    }
+    
+    if (changed) {
       setTimeout(load, 500);
     }
   }
   
-  w.addEventListener('load', load);
+  if (d.querySelector('section.ui-search-results')) {
+    w.addEventListener('load', load);
+  }
 })(document, window);
