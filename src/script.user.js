70055ff74e332aebbdfb5c43733f7d42cf849c40
Ivan <ivan.vladimir.gonzalez@gmail.com>
2021-06-02 22:14:45 -0400

    Borrar "Promocionados"

--- src/script.user.js
+++ src/script.user.js
@@ -3,9 +3,9 @@
 // @namespace   https://mak.wtf/
 // @match       *://*.mercadolibre.cl/*
 // @grant       none
-// @version     1.2.0
+// @version     1.2.1
 // @author      makzk, ivanMSC
-// @description 01/6/2021, 19:27:41 PM
+// @description 02/6/2021, 22:13:00 PM
 // ==/UserScript==
 
 (function(d) {
@@ -13,21 +13,25 @@
   if (!d.querySelector('section.ui-search-results')) return;
   
   (function load () {
-	// <Obsoleto>
-    // Apuntar al flair del tipo de productos, filtrar por los que dicen "Internacional",
-    // obtener el <li> parent, y filtrar los que no hayan sido ocultados.
-	// </Obsoleto>
-	// 20210601: Mercadolibre cambió. Ahora se buscan los divs con clase 
-	// ui-search-item__group.ui-search-item__group--vertical-highlight que contiene
-	// el famoso texto (que no es texto sino que un svg) de compra internacional.
+	// Se buscan los divs con clase ui-search-item__group.ui-search-item__group--vertical-highlight 
+	// que contiene el texto (que no es texto sino que un svg) de compra internacional.
     let results = Array.from(d.querySelectorAll('div.ui-search-item__group.ui-search-item__group--vertical-highlight'))
-      .map(j => j.closest('li.ui-search-layout__item'))
-      .filter(k => k.style.display !== "none");
-    
+		.map(j => j.closest('li.ui-search-layout__item'))
+		.filter(k => k.style.display !== "none");
+		
     // Si se encontraron productos internacionales, ocultarlos y volver a llamar
     if (results.length > 0) {
-      results.forEach(i => { i.style.display = "none" });
-      setTimeout(load, 500);
+		results.forEach(i => { i.style.display = "none" });
+		//setTimeout(load, 500);
     }
+	
+	// Borrar "Promocionado"
+    let resultsPromo = Array.from(d.querySelectorAll('span.ui-search-item__ad-label.ui-search-item__ad-label--blue'))
+		.map(j => j.closest('li.ui-search-layout__item'))
+		.filter(k => k.style.display !== "none");
+    if (resultsPromo.length > 0) {
+		resultsPromo.forEach(i => { i.style.display = "none" });
+    }
+	
   })();
 })(document);
